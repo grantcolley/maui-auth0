cf75075f5a1792b22c2aaa32d482958d05eb3665
grantcolley <grantcolley@yahoo.com>
2022-10-05 20:45:23 +0100

    Adding Auth0 to MauiNativeApp

--- src/MauiNativeApp/MainPage.xaml.cs
+++ src/MauiNativeApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Accessibility;
+﻿using MauiNativeApp.Auth0;
+using Microsoft.Maui.Accessibility;
 using Microsoft.Maui.Controls;
 using System;
 
@@ -8,9 +9,28 @@ namespace MauiNativeApp
     {
         int count = 0;
 
-        public MainPage()
+        private readonly Auth0Client auth0Client;
+
+        public MainPage(Auth0Client client)
         {
             InitializeComponent();
+
+            auth0Client = client;
+        }
+
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            var loginResult = await auth0Client.LoginAsync();
+
+            if (!loginResult.IsError)
+            {
+                LoginView.IsVisible = false;
+                HomeView.IsVisible = true;
+            }
+            else
+            {
+                await DisplayAlert("Error", loginResult.ErrorDescription, "OK");
+            }
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
--- src/MauiNativeApp/MauiProgram.cs
+++ src/MauiNativeApp/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Maui.Controls.Hosting;
+﻿using MauiNativeApp.Auth0;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
 
 namespace MauiNativeApp
@@ -15,6 +17,16 @@ namespace MauiNativeApp
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+            builder.Services.AddSingleton<MainPage>();
+
+            builder.Services.AddSingleton(new Auth0Client(new()
+            {
+                Domain = "<YOUR_AUTH0_DOMAIN>",
+                ClientId = "<YOUR_CLIENT_ID>",
+                Scope = "openid profile",
+                RedirectUri = "myapp://callback"
+            }));
 
             return builder.Build();
         }
