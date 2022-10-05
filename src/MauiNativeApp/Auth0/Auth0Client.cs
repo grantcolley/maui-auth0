e5ab94f4b2be13b5f3da6a205c1b31aa634a038e
grantcolley <grantcolley@yahoo.com>
2022-10-05 19:51:30 +0100

    Add Auth0 classes to MauiNativeApp

---
+++ src/MauiNativeApp/Auth0/Auth0Client.cs
@@ -0,0 +1,39 @@
+﻿using IdentityModel.OidcClient;
+using System.Threading.Tasks;
+
+namespace MauiNativeApp.Auth0
+{
+    public class Auth0Client
+    {
+        private readonly OidcClient oidcClient;
+
+        public Auth0Client(Auth0ClientOptions options)
+        {
+            oidcClient = new OidcClient(new OidcClientOptions
+            {
+                Authority = $"https://{options.Domain}",
+                ClientId = options.ClientId,
+                Scope = options.Scope,
+                RedirectUri = options.RedirectUri,
+                Browser = options.Browser
+            });
+        }
+
+        public IdentityModel.OidcClient.Browser.IBrowser Browser
+        {
+            get
+            {
+                return oidcClient.Options.Browser;
+            }
+            set
+            {
+                oidcClient.Options.Browser = value;
+            }
+        }
+
+        public async Task<LoginResult> LoginAsync()
+        {
+            return await oidcClient.LoginAsync();
+        }
+    }
+}
