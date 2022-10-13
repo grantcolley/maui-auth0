00f4f43702b8b0ec7335b383bc7be72d52ff8c2e
grantcolley <grantcolley@yahoo.com>
2022-10-13 21:02:28 +0100

    Update MauiBlazorApp

--- src/MauiBlazorApp/Auth0/Auth0Client.cs
+++ src/MauiBlazorApp/Auth0/Auth0Client.cs
@@ -1,4 +1,8 @@
-﻿using IdentityModel.OidcClient;
+﻿using IdentityModel.Client;
+using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Browser;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MauiBlazorApp.Auth0
@@ -34,6 +38,28 @@ namespace MauiBlazorApp.Auth0
         public async Task<LoginResult> LoginAsync()
         {
             return await oidcClient.LoginAsync();
+        }
+
+        public async Task<BrowserResult> LogoutAsync()
+        {
+            var logoutParameters = new Dictionary<string, string>
+            {
+                {"client_id", oidcClient.Options.ClientId },
+                {"returnTo", oidcClient.Options.RedirectUri }
+            };
+
+            var logoutRequest = new LogoutRequest();
+            var endSessionUrl = new RequestUrl($"{oidcClient.Options.Authority}/v2/logout")
+              .Create(new Parameters(logoutParameters));
+            var browserOptions = new BrowserOptions(endSessionUrl, oidcClient.Options.RedirectUri)
+            {
+                Timeout = TimeSpan.FromSeconds(logoutRequest.BrowserTimeout),
+                DisplayMode = logoutRequest.BrowserDisplayMode
+            };
+
+            var browserResult = await oidcClient.Options.Browser.InvokeAsync(browserOptions);
+
+            return browserResult;
         }
     }
 }
