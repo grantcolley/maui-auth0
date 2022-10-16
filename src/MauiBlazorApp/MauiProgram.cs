ce60ebc4bda8b9eb9f689e0aba5b13b86f4f928f
grantcolley <grantcolley@yahoo.com>
2022-10-16 19:31:51 +0100

    MauiBlazorApp Auth0

--- src/MauiBlazorApp/MauiProgram.cs
+++ src/MauiBlazorApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using MauiBlazorApp.Data;
+﻿using MauiBlazorApp.Auth0;
+using MauiBlazorApp.Data;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
@@ -22,8 +23,23 @@ public static class MauiProgram
 		#if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		#endif
-		
-		builder.Services.AddSingleton<WeatherForecastService>();
+
+        builder.Services.AddSingleton(new Auth0Client(new Auth0ClientOptions
+        {
+            //Domain = "<YOUR_AUTH0_DOMAIN>",
+            //ClientId = "<YOUR_CLIENT_ID>",
+            Domain = "dev-g5zonud4.us.auth0.com",
+            ClientId = "B85e445eAgYRUnrC7c7XaAXLCeMNyfLQ",
+            Scope = "openid profile",
+
+            // https://github.com/dotnet/maui/issues/8382
+            // RedirectUri = "http://localhost/callback"
+
+            RedirectUri = "myapp://callback"
+
+        }));
+
+        builder.Services.AddSingleton<WeatherForecastService>();
 
 		return builder.Build();
 	}
